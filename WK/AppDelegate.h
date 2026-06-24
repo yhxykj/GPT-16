83675879f4967618b419a5667d581092418d9fd4
过包顺顺顺 <15959281692@163.com>
2026-06-24 18:23:11 +0800

    Add project files part 1

---
+++ WK/AppDelegate.h
@@ -0,0 +1,16 @@
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property(nonatomic, assign)NSInteger  changSum;
+@property(nonatomic, assign)double  address_min;
+@property(nonatomic, copy)NSArray *  codeLayerPolicy_arr;
+@property(nonatomic, assign)double  launch_size;
+
+
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
+
---
+++ WK/AppDelegate.m
@@ -0,0 +1,222 @@
+
+#import "AppDelegate.h"
+#import "WKDetailsController.h"
+#import "WKTellDetailsController.h"
+#import "WKLoadingController.h"
+
+
+@interface AppDelegate ()
+
+
+@property(nonatomic, assign)int  matchMark;
+@property(nonatomic, assign)int  kuaisuIndex;
+@property(nonatomic, assign)BOOL  hasAry;
+@property(nonatomic, assign)double  isfirstMin;
+
+
+@end
+
+@implementation AppDelegate
+
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+       unsigned char tiaoguo8[] = {75,150,204,76};
+    char fitsY[] = {120,(char)-102,13,(char)-12,122,88,40,49,(char)-102,(char)-105};
+   if ((tiaoguo8[0] + fitsY[2]) == 3 || (3 + tiaoguo8[0]) == 4) {
+      long icon5 = sizeof(tiaoguo8) / sizeof(tiaoguo8[0]);
+      tiaoguo8[3] ^= (1 + icon5) & fitsY[7];
+   }
+
+   self.matchMark = 4140;
+
+   self.kuaisuIndex = 8180;
+
+   self.hasAry = NO;
+
+   self.isfirstMin = 2565.0;
+
+   self.changSum = 9977;
+
+   self.address_min = 9325.0;
+
+   self.codeLayerPolicy_arr = [NSArray arrayWithObjects:@(206), @(792), nil];
+
+   self.launch_size = 7407.0;
+
+    IQKeyboardManager.sharedManager.enable = true;
+   while ((fitsY[3] / (MAX(4, 1))) <= 3 && 4 <= (4 / (MAX(8, tiaoguo8[1])))) {
+       int vipd = 2;
+       char socket8[] = {66,118,(char)-25,58,61,(char)-9};
+       unsigned char time_iX[] = {76,2};
+      while (3 == (time_iX[0] >> (MIN(labs(4), 1))) && (socket8[0] >> (MIN(labs(4), 3))) == 5) {
+         time_iX[1] -= time_iX[0];
+         break;
+      }
+       double wenant = 5.0f;
+      while (time_iX[1] <= socket8[1]) {
+         socket8[3] >>= MIN(2, labs(vipd));
+         break;
+      }
+      if ((wenant / 1) > 2 && 3 > (1 / (MAX(4, wenant)))) {
+         long headerQ = sizeof(time_iX) / sizeof(time_iX[0]);
+         wenant -= (2 + headerQ) >> (MIN(labs(socket8[4]), 5));
+      }
+       long prefix_f0 = 0;
+      do {
+          long vewZ = 1;
+          NSDictionary * btnC = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){82,0}],[NSString stringWithUTF8String:(char []){67,0}], nil];
+         long fuzhi4 = sizeof(time_iX) / sizeof(time_iX[0]);
+         vipd |= fuzhi4;
+         vewZ >>= MIN(labs(btnC.count << (MIN(labs(2), 3))), 5);
+         vewZ %= MAX(4, btnC.count);
+         if (vipd == 3481396) {
+            break;
+         }
+      } while (((vipd - prefix_f0) >= 1 || (vipd - prefix_f0) >= 1) && (vipd == 3481396));
+       double guanbiN = 3.0f;
+       double t_centerj = 1.0f;
+      if (1 > (guanbiN + 1)) {
+         socket8[MAX(4, prefix_f0 % 6)] /= MAX(1, 1);
+      }
+      for (int m = 0; m < 1; m++) {
+         socket8[0] &= vipd;
+      }
+      fitsY[MAX(2, vipd % 10)] %= MAX(3, 1);
+      break;
+   }
+    IQKeyboardManager.sharedManager.shouldResignOnTouchOutside = true;
+   for (int q = 0; q < 2; q++) {
+       unsigned char checkO[] = {227,22,37,51,43,159};
+       NSDictionary * x_imageR = @{[NSString stringWithUTF8String:(char []){114,101,110,100,101,114,105,110,103,0}]:@(875).stringValue};
+       NSInteger purchaseG = 5;
+       char interesty[] = {(char)-2,(char)-13,90,107};
+       char checkN[] = {39,106,(char)-31,(char)-100,34,21,72,67,117,52};
+      while ((2 + x_imageR.allKeys.count) > 3 && (x_imageR.allKeys.count + checkN[5]) > 2) {
+         checkN[2] &= 2 - checkN[4];
+         break;
+      }
+       double zhulis = 4.0f;
+      for (int a = 0; a < 2; a++) {
+          float answer_ = 0.0f;
+          NSArray * w_layere = @[[NSString stringWithUTF8String:(char []){98,101,104,97,118,105,111,114,115,0}], [NSString stringWithUTF8String:(char []){97,115,115,105,103,110,109,101,110,116,0}], [NSString stringWithUTF8String:(char []){114,101,109,111,118,105,110,103,0}]];
+          NSString * yinsih = [NSString stringWithUTF8String:(char []){99,114,121,112,116,0}];
+          int historyb = 1;
+         purchaseG %= MAX(x_imageR.count >> (MIN(labs(1), 3)), 5);
+         answer_ -= 3 & historyb;
+         historyb %= MAX(w_layere.count / (MAX(2, 6)), 1);
+         historyb *= 4 * yinsih.length;
+         historyb %= MAX(w_layere.count, 1);
+         historyb |= yinsih.length << (MIN(labs(5), 5));
+      }
+      while (4 <= (4 << (MIN(5, labs(purchaseG))))) {
+          int plugino = 2;
+         purchaseG += purchaseG;
+         plugino <<= MIN(labs(3), 4);
+         break;
+      }
+       NSDictionary * defalutZ = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){108,101,97,118,101,0}],@(302), [NSString stringWithUTF8String:(char []){112,105,110,103,0}],@(516), [NSString stringWithUTF8String:(char []){110,105,109,97,116,101,100,0}],@(738), nil];
+       NSArray * placeholderlabel8 = [NSArray arrayWithObjects:[NSString stringWithUTF8String:(char []){115,111,108,105,100,0}], nil];
+       NSArray * speedg = [NSArray arrayWithObjects:[NSString stringWithUTF8String:(char []){115,101,108,101,99,116,111,112,0}], nil];
+      for (int m = 0; m < 1; m++) {
+         purchaseG *= speedg.count;
+      }
+      for (int c = 0; c < 1; c++) {
+          double register_okc = 0.0f;
+          NSInteger successl = 0;
+          char sectionsU[] = {23,(char)-108,80,2};
+          char socketH[] = {10,(char)-43,(char)-114};
+         zhulis *= (int)register_okc;
+         register_okc -= successl;
+         NSInteger zuanshi6 = sizeof(socketH) / sizeof(socketH[0]);
+         successl %= MAX(3, zuanshi6);
+         sectionsU[MAX(successl % 4, 0)] ^= successl;
+      }
+      if ((interesty[1] | x_imageR.count) == 2) {
+          NSDictionary * requestc = [NSDictionary dictionaryWithObjectsAndKeys:[NSString stringWithUTF8String:(char []){99,111,110,116,105,110,117,101,0}],@(125).stringValue, nil];
+          NSInteger youhuiU = 2;
+          double danjia9 = 5.0f;
+          int failedx = 1;
+          NSInteger stillfor0 = 0;
+         youhuiU &= x_imageR.count;
+         failedx *= requestc.count;
+         youhuiU /= MAX(5, failedx * stillfor0);
+         danjia9 *= (int)danjia9;
+         stillfor0 *= 1 << (MIN(labs(failedx), 4));
+         stillfor0 |= 1 ^ requestc.count;
+      }
+      do {
+          NSInteger monthly4 = 0;
+          char g_centerg[] = {(char)-66,(char)-22,42,3,123,(char)-9,113,75,(char)-17,97,(char)-102,(char)-10};
+          BOOL titlesD = NO;
+         monthly4 |= placeholderlabel8.count;
+         monthly4 %= MAX(3, 1 ^ g_centerg[4]);
+         g_centerg[6] /= MAX(1, (2 >> (MIN(1, labs((titlesD ? 1 : 1))))));
+         if (placeholderlabel8.count == 983525) {
+            break;
+         }
+      } while ((placeholderlabel8.count == 983525) && (1 < (4 / (MAX(10, speedg.count)))));
+       double description_ft8 = 5.0f;
+       double fasongd = 0.0f;
+      do {
+         purchaseG *= x_imageR.count * 2;
+         if (x_imageR.count == 3899014) {
+            break;
+         }
+      } while ((x_imageR.count == 3899014) && ((3 + x_imageR.allKeys.count) < 1 && (fasongd / (MAX(x_imageR.allKeys.count, 2))) < 1.16f));
+      do {
+         purchaseG &= x_imageR.count % 1;
+         if (816996 == x_imageR.count) {
+            break;
+         }
+      } while ((816996 == x_imageR.count) && (x_imageR[@(purchaseG).stringValue]));
+      if (1 <= (3 ^ interesty[0]) || (zhulis + interesty[0]) <= 3) {
+          char answerv[] = {(char)-105,(char)-128,(char)-11,120,(char)-91,(char)-125,(char)-24,(char)-103,(char)-58,54,11,85};
+          NSString * speechq = [NSString stringWithUTF8String:(char []){105,112,111,108,0}];
+          double labeld = 2.0f;
+          unsigned char videoS[] = {185,4,168,150,69,192};
+          unsigned char iteme[] = {184,69,9,146,20};
+         NSInteger presentationY = sizeof(iteme) / sizeof(iteme[0]);
+         zhulis += (int)labeld % (MAX(presentationY, 10));
+         answerv[0] <<= MIN(5, labs(videoS[1] + speechq.length));
+         labeld -= videoS[2];
+      }
+      for (int a = 0; a < 2; a++) {
+         fasongd -= 2;
+      }
+      long localeq = sizeof(tiaoguo8) / sizeof(tiaoguo8[0]);
+      tiaoguo8[1] /= MAX(2, localeq);
+      long timerk = sizeof(checkO) / sizeof(checkO[0]);
+      checkO[5] ^= checkO[5] >> (MIN(5, labs(timerk)));
+   }
+    [SVProgressHUD setMinimumDismissTimeInterval:1.2];
+    [self.window makeKeyAndVisible];
+    [self.window makeKeyWindow];
+    
+    
+    [WKDetailsReusable.data getNetWork];
+    NSString *back = [[NSUserDefaults standardUserDefaults] objectForKey:@"token"];
+    
+    if ([[SAVE_UDF objectForKey:@"enterApp"] boolValue] == false) {
+        WKTellDetailsController *receiveController = WKTellDetailsController.new;
+        UINavigationController *destructionView = [[UINavigationController alloc] initWithRootViewController:receiveController];
+        [self.window setRootViewController:destructionView];
+    }
+    
+    else if (back.length == 0) {
+        WKPresentationController *receiveController = WKPresentationController.new;
+        UINavigationController *destructionView = [[UINavigationController alloc] initWithRootViewController:receiveController];
+        [self.window setRootViewController:destructionView];
+    }
+    
+    else {
+        WKDetailsController *receiveController = WKDetailsController.new;
+        UINavigationController *destructionView = [[UINavigationController alloc] initWithRootViewController:receiveController];
+        [self.window setRootViewController:destructionView];
+    }
+    
+    return YES;
+}
+
+
+@end
